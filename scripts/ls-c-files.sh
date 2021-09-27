462c4e81f10171f0d7d5ff767ca48b1c0932a6cc
Geoffrey Gilmore <me@ggilmore.net>
2021-09-26 19:51:02 -0700

    format

--- scripts/ls-c-files.sh
+++ scripts/ls-c-files.sh
@@ -9,3 +9,4 @@ IGNORE_DIRS=(
 REPOSITORY_ROOT_RELATIVE_PATH="$(realpath --relative-to="$(pwd)" "$(dirname "${BASH_SOURCE[0]}")"/..)"
 
 fd --extension c --exclude "${IGNORE_DIRS[@]}" . "${REPOSITORY_ROOT_RELATIVE_PATH}"
+fd --extension h --exclude "${IGNORE_DIRS[@]}" . "${REPOSITORY_ROOT_RELATIVE_PATH}"
