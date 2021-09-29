fefc41fd00b279f6b892c484d7617a7319fffe72
Geoffrey Gilmore <me@ggilmore.net>
2021-09-28 19:17:43 -0700

    add matrix solution

--- src/classes/intro-systems/assembly2/prework/matrix.asm
+++ src/classes/intro-systems/assembly2/prework/matrix.asm
@@ -6,4 +6,10 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+
+    mov rax, rdx
+    imul rax, rcx ; cols * rindex
+    add rax, r8 ; cols * rindex + cindex = offset
+
+    mov rax, [rdi + rax * 4]; offset * 4 bytes per int
 	ret
