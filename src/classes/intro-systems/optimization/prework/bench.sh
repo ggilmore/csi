040a2adede456eaa25748a7f345d13b7394a043c
Geoffrey Gilmore <tkdmaster123@gmail.com>
2021-10-04 14:51:12 -0700

    fix bench script for pagecount (#7)

--- src/classes/intro-systems/optimization/prework/bench.sh
+++ src/classes/intro-systems/optimization/prework/bench.sh
@@ -3,19 +3,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -euxo pipefail
 
-# function my_gcc() {
-#   if [[ -f "/usr/local/bin/gcc-11" ]]; then
-#     "/usr/local/bin/gcc-11" "$@"
-#     return
-#   fi
-
-#   gcc "$@"
-# }
-
-# my_gcc -Og -S mstore.c
-# my_gcc -Og -c mstore.c
-# my_gcc -Og -o prog main.c mstore.c
-
 ./build.sh
 
-hyperfine -i --warmup=500 --min-runs=1000 'pagecount_orig.o' 'pagecount_ctz.o' 'pagecount_encoding.o'
+hyperfine -i --export-markdown=pagecount_timing.md './pagecount_orig.o' './pagecount_ctz.o' './pagecount_encoding.o'
