0bafca07cd0ee8c72dfe8b195b418da48a749426
Geoffrey Gilmore <tkdmaster123@gmail.com>
2021-10-25 14:29:08 -0400

    add rest of unsafe functions (#10)

--- src/classes/advanced-programing/class1/main_test.go
+++ src/classes/advanced-programing/class1/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
@@ -36,4 +37,56 @@ func TestStructField(t *testing.T) {
 			t.Errorf("for (%+v).Y got: %d, wanted: %d", input, actual, expected)
 		}
 	}
+}
+func TestSumSlice(t *testing.T) {
+	var input []int
+	f := fuzz.New().NumElements(0, 10)
+
+	for i := 0; i < 1000; i++ {
+		f.Fuzz(&input)
+
+		expected := correctSumSlice(input)
+		actual := SumSlice(input)
+
+		if actual != expected {
+			t.Errorf("for %+v got: %d, wanted: %d", input, actual, expected)
+		}
+	}
+}
+
+func TestMapMax(t *testing.T) {
+	var input map[int]int
+	f := fuzz.New().NumElements(0, 10)
+
+	for i := 0; i < 1; i++ {
+		f.Fuzz(&input)
+
+		expected := correctMapMax(input)
+		actual := MapMax(input)
+
+		if actual != expected {
+			t.Errorf("for %+v got: %d, wanted: %d", input, actual, expected)
+		}
+	}
+}
+
+func correctSumSlice(xs []int) int {
+	sum := 0
+	for _, x := range xs {
+		sum += x
+	}
+
+	return sum
+}
+
+func correctMapMax(xs map[int]int) int {
+	max := math.MinInt
+
+	for _, v := range xs {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
 }
