632ce2c9fa95cfc8387138e39fedf96a748f82c5
Geoffrey Gilmore <me@ggilmore.net>
2021-10-30 14:04:39 -0700

    example 5

--- src/classes/advanced-programing/concurrency/prework/example5/example-5.go
+++ src/classes/advanced-programing/concurrency/prework/example5/example-5.go
@@ -25,19 +25,65 @@ func query(endpoint string) string {
 	return responses[i]
 }
 
+// Original:
+// // Query each of the mirrors in parallel and return the first
+// // response (this approach increases the amount of traffic but
+// // significantly improves "tail latency")
+// func parallelQuery(endpoints []string) string {
+// 	results := make(chan string)
+// 	for i := range endpoints {
+// 		go func(i int) {
+// 			results <- query(endpoints[i])
+// 		}(i)
+// 	}
+// 	return <-results
+// }
+
+// func main() {
+// 	var endpoints = []string{
+// 		"https://fakeurl.com/endpoint",
+// 		"https://mirror1.com/endpoint",
+// 		"https://mirror2.com/endpoint",
+// 	}
+
+// 	// Simulate long-running server process that makes continuous queries
+// 	for {
+// 		fmt.Println(parallelQuery(endpoints))
+// 		delay := randomDelay(100)
+// 		time.Sleep(delay)
+// 	}
+// }
+
 // Query each of the mirrors in parallel and return the first
 // response (this approach increases the amount of traffic but
 // significantly improves "tail latency")
-func parallelQuery(endpoints []string) string {
-	results := make(chan string)
+func parallelQuery(endpoints []string) (endpoint, response string) {
+	type result struct {
+		endpoint string
+		response string
+	}
+
+	results := make(chan result, len(endpoints))
 	for i := range endpoints {
 		go func(i int) {
-			results <- query(endpoints[i])
+			endpoint := endpoints[i]
+			response := query(endpoint)
+			results <- result{
+				endpoint: endpoint,
+				response: response,
+			}
+			fmt.Println("done sending ")
 		}(i)
 	}
-	return <-results
+
+	r := <-results
+	return r.endpoint, r.response
 }
 
+// The results channel was unbuffered, which resulted in a goroutine leak.
+// Buffering the channel fixed the issue (and lets the goroutines return).
+// Another way to solve it would be to have some kind of context.Done or
+// other signal that lets them return
 func main() {
 	var endpoints = []string{
 		"https://fakeurl.com/endpoint",
@@ -47,7 +93,8 @@ func main() {
 
 	// Simulate long-running server process that makes continuous queries
 	for {
-		fmt.Println(parallelQuery(endpoints))
+		endpoint, response := parallelQuery(endpoints)
+		fmt.Printf("%q: %s\n", endpoint, response)
 		delay := randomDelay(100)
 		time.Sleep(delay)
 	}
