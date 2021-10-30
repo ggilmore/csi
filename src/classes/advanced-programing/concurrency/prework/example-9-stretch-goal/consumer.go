70ed7eb78f7b06f3fda16d89d8b09c31a7478e7a
Geoffrey Gilmore <me@ggilmore.net>
2021-10-30 14:59:17 -0700

    fix 9

--- src/classes/advanced-programing/concurrency/prework/example-9-stretch-goal/consumer.go
+++ src/classes/advanced-programing/concurrency/prework/example-9-stretch-goal/consumer.go
@@ -27,12 +27,12 @@ func (c *Consumer) GetState() string {
 }
 
 func (c *Consumer) Terminate() {
+	c.s.RemoveConsumer(c.id)
+
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
 	// You can imagine that this internal cleanup mutates the state
 	// of the Consumer
 	fmt.Printf("Performing internal cleanup for consumer %d\n", c.id)
-
-	c.s.RemoveConsumer(c.id)
 }
