d2202178bc686537ae6cd85ec83da4b28839f667
Geoffrey Gilmore <me@ggilmore.net>
2021-10-30 14:14:20 -0700

    example 6

--- src/classes/advanced-programing/concurrency/prework/example6/example-6.go
+++ src/classes/advanced-programing/concurrency/prework/example6/example-6.go
@@ -5,6 +5,37 @@ import (
 	"sync"
 )
 
+// Original
+// type coordinator struct {
+// 	lock   sync.RWMutex
+// 	leader string
+// }
+
+// func newCoordinator(leader string) *coordinator {
+// 	return &coordinator{
+// 		lock:   sync.RWMutex{},
+// 		leader: leader,
+// 	}
+// }
+
+// func (c *coordinator) logState() {
+// 	c.lock.RLock()
+// 	defer c.lock.RUnlock()
+
+// 	fmt.Printf("leader = %q\n", c.leader)
+// }
+
+// func (c *coordinator) setLeader(leader string, shouldLog bool) {
+// 	c.lock.Lock()
+// 	defer c.lock.Unlock()
+
+// 	c.leader = leader
+
+// 	if shouldLog {
+// 		c.logState()
+// 	}
+// }
+
 type coordinator struct {
 	lock   sync.RWMutex
 	leader string
@@ -17,26 +48,40 @@ func newCoordinator(leader string) *coordinator {
 	}
 }
 
-func (c *coordinator) logState() {
+func (c *coordinator) LogState() {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
+	c.doLogRaw()
+}
+
+func (c *coordinator) doLogRaw() {
 	fmt.Printf("leader = %q\n", c.leader)
 }
 
-func (c *coordinator) setLeader(leader string, shouldLog bool) {
+func (c *coordinator) SetLeader(leader string, shouldLog bool) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
 	c.leader = leader
 
 	if shouldLog {
-		c.logState()
+		c.doLogRaw()
 	}
 }
 
+// The old setLeader function was trying to acquire a read lock
+// when it already held a write lock. That operation would never
+// complete, resulting in deadlock.
+//
+// I refactored the logging code to have an unexported function
+// just write to stdout without trying to hold a lock. It's the
+// caller's responsibility to hold the lock if necessary.
+//
+// The now-exported LogState and SetLeader are now the public facing
+// ways trigger a log, and they each make sure to hold the lock.
 func main() {
 	c := newCoordinator("us-east")
-	c.logState()
-	c.setLeader("us-west", true)
+	c.LogState()
+	c.SetLeader("us-west", true)
 }
