ab4cabe7a0a25d24ea039979544dda89d3e5b548
Geoffrey Gilmore <me@ggilmore.net>
2021-10-30 14:33:16 -0700

    example 8

--- src/classes/advanced-programing/concurrency/prework/example8/example-8.go
+++ src/classes/advanced-programing/concurrency/prework/example8/example-8.go
@@ -6,6 +6,71 @@ import (
 	"time"
 )
 
+// Original:
+//
+// type dbService struct {
+// 	lock       *sync.RWMutex
+// 	connection string
+// }
+
+// func newDbService(connection string) *dbService {
+// 	return &dbService{
+// 		lock:       &sync.RWMutex{},
+// 		connection: connection,
+// 	}
+// }
+
+// func (d *dbService) logState() {
+// 	d.lock.RLock()
+// 	defer d.lock.RUnlock()
+
+// 	fmt.Printf("connection %q is healthy\n", d.connection)
+// }
+
+// func (d *dbService) takeSnapshot() {
+// 	d.lock.RLock()
+// 	defer d.lock.RUnlock()
+
+// 	fmt.Printf("Taking snapshot over connection %q\n", d.connection)
+
+// 	// Simulate slow operation
+// 	time.Sleep(time.Second)
+
+// 	d.logState()
+// }
+
+// func (d *dbService) updateConnection(connection string) {
+// 	d.lock.Lock()
+// 	defer d.lock.Unlock()
+
+// 	d.connection = connection
+// }
+
+// func main() {
+// 	d := newDbService("127.0.0.1:3001")
+
+// 	var wg sync.WaitGroup
+
+// 	wg.Add(1)
+// 	go func() {
+// 		defer wg.Done()
+
+// 		d.takeSnapshot()
+// 	}()
+
+// 	// Simulate other DB accesses
+// 	time.Sleep(200 * time.Millisecond)
+
+// 	wg.Add(1)
+// 	go func() {
+// 		defer wg.Done()
+
+// 		d.updateConnection("127.0.0.1:8080")
+// 	}()
+
+// 	wg.Wait()
+// }
+
 type dbService struct {
 	lock       *sync.RWMutex
 	connection string
@@ -18,10 +83,14 @@ func newDbService(connection string) *dbService {
 	}
 }
 
-func (d *dbService) logState() {
+func (d *dbService) LogState() {
 	d.lock.RLock()
 	defer d.lock.RUnlock()
 
+	d.dologRaw()
+}
+
+func (d *dbService) dologRaw() {
 	fmt.Printf("connection %q is healthy\n", d.connection)
 }
 
@@ -34,7 +103,7 @@ func (d *dbService) takeSnapshot() {
 	// Simulate slow operation
 	time.Sleep(time.Second)
 
-	d.logState()
+	d.dologRaw()
 }
 
 func (d *dbService) updateConnection(connection string) {
@@ -44,6 +113,8 @@ func (d *dbService) updateConnection(connection string) {
 	d.connection = connection
 }
 
+// The first goroutine was acquiring the same readlock twice (reentrant).
+// Using the same technique as problem 6 fixes the issue.
 func main() {
 	d := newDbService("127.0.0.1:3001")
 
