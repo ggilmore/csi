edf763898cb7756fc40c203e115280b9af1aa3ed
Geoffrey Gilmore <me@ggilmore.net>
2021-10-30 14:18:08 -0700

    fix 7

--- src/classes/advanced-programing/concurrency/prework/example7/example-7.go
+++ src/classes/advanced-programing/concurrency/prework/example7/example-7.go
@@ -10,17 +10,55 @@ const (
 	numIncrements = 100
 )
 
+// Original:
+// type counter struct {
+// 	count int
+// }
+
+// func safeIncrement(lock sync.Mutex, c *counter) {
+// 	lock.Lock()
+// 	defer lock.Unlock()
+
+// 	c.count += 1
+// }
+
+// func main() {
+// 	var globalLock sync.Mutex
+// 	c := &counter{
+// 		count: 0,
+// 	}
+
+// 	var wg sync.WaitGroup
+// 	for i := 0; i < numGoroutines; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+
+// 			for j := 0; j < numIncrements; j++ {
+// 				safeIncrement(globalLock, c)
+// 			}
+// 		}()
+// 	}
+
+// 	wg.Wait()
+// 	fmt.Println(c.count)
+// }
+
 type counter struct {
 	count int
 }
 
-func safeIncrement(lock sync.Mutex, c *counter) {
+func safeIncrement(lock sync.Locker, c *counter) {
 	lock.Lock()
 	defer lock.Unlock()
 
-	c.count += 1
+	c.count++
 }
 
+// The old code was passing the globalLock value, not a pointer to it.
+// This means that this was equivalent to each goroutine having their
+// individual lock (which is effectively useless).
+// Chaning this to a pointer reference fixes the issue.
 func main() {
 	var globalLock sync.Mutex
 	c := &counter{
@@ -34,7 +72,7 @@ func main() {
 			defer wg.Done()
 
 			for j := 0; j < numIncrements; j++ {
-				safeIncrement(globalLock, c)
+				safeIncrement(&globalLock, c)
 			}
 		}()
 	}
