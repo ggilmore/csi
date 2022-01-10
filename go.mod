210fa360be1d38d322519804ecc00742e759945b
Geoffrey Gilmore <me@ggilmore.net>
2022-01-09 16:26:57 -0800

    wip

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/ggilmore/csi
 
 go 1.17
-
-require (
-	github.com/google/gofuzz v1.2.0
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-)
--- src/classes/advanced-programing/networking/dns/prework/record.go
+++ src/classes/advanced-programing/networking/dns/prework/record.go
@@ -1,13 +1,26 @@
 package main
 
+import "io"
+
 type Record struct {
 	rType    uint16
 	class    uint16
-	ttl      int32
+	TTL      int32
 	rdLength uint16
 	rData    string
 }
 
+func Encode(r Record, w io.Writer) error {
+	return nil
+}
+
+func (r Record) ResourceType() ResourceType {
+	return ResourceType(r.rType)
+}
+
+func (r Record) ResourceClass() ResourceClass{
+	return ResourceClass(r.class)
+}
 type ResourceType int
 
 const (
@@ -27,4 +40,13 @@ const (
 	ResourceTypeMINFO
 	ResourceTypeMX
 	ResourceTypeTXT
+)
+
+type ResourceClass int
+
+const (
+	ResourceClassIN ResourceClass = iota + 1
+	ResourceClassCS
+	ResourceClassCH
+	ResourceClassHS
 )
